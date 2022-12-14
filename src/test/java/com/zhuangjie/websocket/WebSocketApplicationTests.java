45baec8103ad0ed08fe4194b480c12e8b958d124
一只喵zjazn <2119299531@qq.com>
2022-12-14 15:54:52 +0800

    优化样式与解决打包报错

--- src/test/java/com/zhuangjie/websocket/WebSocketApplicationTests.java
+++ src/test/java/com/zhuangjie/websocket/WebSocketApplicationTests.java
@@ -3,7 +3,8 @@ package com.zhuangjie.websocket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// 不加 webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT 时，打包会报错
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebSocketApplicationTests {
 
     @Test
